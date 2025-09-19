187fe65fd753af5c50657ec1e0ce9635f96b1882
sierra-adkins <adkins67@purdue.edu>
2025-09-19 09:00:09 -0400

    Add files via upload

---
+++ POM.py
@@ -0,0 +1,8 @@
+echo "# PHARM-POM-group-file" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Rohankumar142/PHARM-POM-group-file.git
+git push -u origin main
+
